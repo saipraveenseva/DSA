f651e7ecf501b533249c64c8c2d0e895d8a99e51
saipraveenseva <saipraveenseva1111@gmail.com>
2023-06-10 22:16:45 +0530

    Intitial version

--- StriverArrays/src/MyFirstArray.java
+++ StriverArrays/src/MyFirstArray.java
@@ -1,5 +1,10 @@
+
+
 public class MyFirstArray {
 
 
     //Here is my first array.
+    public static void main(String[] args) {
+        System.out.println("Hi, This is my first array..!!y");
+    }
 }
