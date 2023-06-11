062f294e56a45735c69656623f60d91187d80702
saipraveenseva <saipraveenseva1111@gmail.com>
2023-06-11 10:56:26 +0530

    Intitial version

--- Patterns/src/SP2.java
+++ Patterns/src/SP2.java
@@ -12,9 +12,9 @@ public class SP2 {
         /*j loop runs i times every loop
         * if i=0, j run 1 time(0)
         * if i=1 j runs 2 times(0,1)
-        * if i=2 j runs 3 times(0,1,2,3)
-        * if i=3 j runs 4 times(0,1,2,3,4)
-        * if i=4 j runs 5 times(0,1,2,3,4,5)
+        * if i=2 j runs 3 times(0,1,2)
+        * if i=3 j runs 4 times(0,1,2,3)
+        * if i=4 j runs 5 times(0,1,2,3,4)
         * Once i hits 5 i loop terminates, terminating j loop as well.*/
 
         //Display section
