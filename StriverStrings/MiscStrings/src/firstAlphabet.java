91ab70cb8a247e403e0042a28a741af3df646863
saipraveenseva <saipraveenseva@gmail.com>
2023-08-22 18:06:50 +0530

    initial version

--- StriverStrings/MiscStrings/src/firstAlphabet.java
+++ StriverStrings/MiscStrings/src/firstAlphabet.java
@@ -1,9 +1,18 @@
+// Write a function that displays first letter of each word in a sentence
+// "Sai praveen Seva" return Sps
+
+
+
 public class firstAlphabet {
     String firstLetter(String S) {
-        // code here
-
         StringBuilder str = new StringBuilder();
 
+        //Logic Section
+        /* Its best to use a StringBuilder since we have to return a string
+            we have to print the first letter so we use i==0 condition
+            For the rest we just search for a space(' ') and display the i+1th character
+            We append the first alphabet of each word into str and then return it.
+         */
         for(int i=0;i<S.length();i++){
             if(i==0)
             {
