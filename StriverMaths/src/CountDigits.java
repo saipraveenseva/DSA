e9c451f22cc6f66d6bb1d6124d0f2ab3fd605f4f
saipraveenseva <saipraveenseva@gmail.com>
2023-10-16 20:09:45 +0530

    initial version

--- StriverMaths/src/CountDigits.java
+++ StriverMaths/src/CountDigits.java
@@ -2,13 +2,24 @@ public class CountDigits {
 
     public static void main(String[] args) {
 
-        long n=1234567890;
+        int n=980;
+        int d=n;
+        int p;
         int count=0;
-        while(n>0){
-            n=n/10;
-            count++;
+        int divcount=0;
+        while(d>0){
+            p=d%10;
+            if(p!=0) { // To avoid dive by zero exception
+                if (n % p == 0) { //Dividing n with each digit, if it is divisible we do divcount++
+                    divcount++;
+                }
+            }
+            d=d/10;
+            count++; // This just counts the number of digits.
+
         }
         System.out.println("Digit count: "+count);
+        System.out.println("Div count: "+divcount);
     }
 
 }
