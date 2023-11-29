4783aa26e88101d911ac8acc2f57be152caf56c3
saipraveenseva <saipraveenseva@gmail.com>
2023-11-29 19:46:58 +0530

    initial

---
+++ StriverArrays/src/MoveZeroesRight.java
@@ -0,0 +1,44 @@
+public class MoveZeroesRight {
+
+    public static int[] MoveZeroes(int arr[]){
+
+        int j=-1;
+        for(int p=0;p<arr.length;p++){
+            if(arr[p]==0){
+                j=p;
+                break;
+            }
+        }
+
+
+        if(j==-1){
+            return arr;
+        }
+        else{
+
+            for(int i=j+1;i<arr.length;i++){
+                if(arr[i]!=0){
+                    arr[i]=arr[i]+arr[j];
+                    arr[j]=arr[i]-arr[j];
+                    arr[i]=arr[i]-arr[j];
+
+                    j++;
+                }
+            }
+            return arr;
+        }
+
+    }
+
+
+
+    public static void main(String[] args) {
+            int arr[]={1,0,2,3,2,0,0,4,5,1};
+
+            MoveZeroes(arr);
+
+            for(int i=0;i<arr.length;i++){
+                System.out.print(arr[i]+" ");
+            }
+    }
+}
