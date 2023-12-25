eb9f30e782eaf9cadbdf2eeffb1cfe1b51d966eb
saipraveenseva <saipraveenseva@gmail.com>
2023-12-25 14:16:41 +0530

    initial

---
+++ Misc/src/GenerateSubArraysSum.java
@@ -0,0 +1,25 @@
+public class GenerateSubArraysSum {
+
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        int n=arr.length;
+        int sum=0;
+        for(int i=0;i<n;i++){                           // Runs from 0 to n-1
+            for(int j=i;j<n;j++){                       // Runs from i to n-1
+                sum=0;
+                for(int k=i;k<=j;k++) {
+
+                    System.out.print(arr[k] + " ");     // Prints from i to j-1 and increments j
+                    sum = sum + arr[k];
+                }
+                System.out.print("sum: " + sum);
+                if(sum==9){
+                    return;         // Displays the sub array with sum K.
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
+}
--- StriverMaths/src/CountDigits.java
+++ StriverMaths/src/CountDigits.java
@@ -9,7 +9,7 @@ public class CountDigits {
         int divcount=0;
         while(d>0){
             p=d%10;
-            if(p!=0) { // To avoid dive by zero exception
+            if(p!=0) { // To avoid divide by zero exception
                 if (n % p == 0) { //Dividing n with each digit, if it is divisible we do divcount++ hello
                     divcount++;
                 }
